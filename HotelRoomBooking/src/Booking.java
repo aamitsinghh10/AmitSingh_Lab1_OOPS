f2fefe0d4162e27b36e3c904be8570ebb488099c
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2022-09-24 12:10:49 +0530

    Update Booking.java

--- HotelRoomBooking/src/Booking.java
+++ HotelRoomBooking/src/Booking.java
@@ -2,13 +2,11 @@ public class Booking {
     private Room room;
     private Customer customer;
     private int totalPrice;
-    private StayDuration duration;
 
-    public Booking(Room room,Customer customer, int totalPrice, StayDuration duration){
+    public Booking(Room room,Customer customer, int totalPrice){
         this.room = room;
         this.customer = customer;
         this.totalPrice = totalPrice;
-        this.duration = duration;
     }
 
     public Room getRoom() {
@@ -33,13 +31,5 @@ public class Booking {
 
     public void setTotalPrice(int totalPrice) {
         this.totalPrice = totalPrice;
-    }
-
-    public StayDuration getDuration() {
-        return duration;
-    }
-
-    public void setDuration(StayDuration duration) {
-        this.duration = duration;
     }
 }
